7bafdb4667a5ff7d331fa391356028225aa0b3a2
Zerotask <Zerotask@users.noreply.github.com>
2021-04-20 18:58:21 +0200

    refactor(main): moved functions

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,5 @@
+const config = require('../config.json');
+
 const getRandomNumber = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 const getUserFromMention = (client, mention) => {
@@ -33,6 +35,44 @@ const shuffleArray = (array) => {
 
 const isMod = (member) => member.roles.cache.some((role) => role.name === 'Mod');
 
+const reactToEmojis = (message) => {
+  const lowerCaseContent = message.content.toLowerCase();
+  if (lowerCaseContent.includes('kappa')) {
+    message.channel.send('https://i.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large');
+  } else if (lowerCaseContent.includes('lul')) {
+    message.channel.send('https://freepngimg.com/thumb/league_of_legends/85483-twitch-emote-face-facial-john-expression-bain.png');
+  } else if (lowerCaseContent.includes('pogchamp')) {
+    message.channel.send('https://freepngimg.com/thumb/mouth/92712-ear-head-twitch-pogchamp-emote-free-download-png-hq-thumb.png');
+  } else if (lowerCaseContent.includes('feelsbadman')) {
+    message.channel.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt9RC-fEDF3xBlO3rRUGFzdX3T25ipBlHq9Q&usqp=CAU');
+  } else if (lowerCaseContent.includes('🥳')) {
+    message.channel.send(':partying_face: :partying_face: :partying_face:');
+  }
+};
+
+const reactToCommands = (client, message) => {
+  // Besides reacting to emojis, ignore all non-commands.
+  if (!message.content.startsWith(config.commandPrefix)) {
+    return;
+  }
+
+  const args = message.content.slice(config.commandPrefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  try {
+    const cmd = client.commands.get(command)
+      || client.commands.find((entry) => entry.aliases && entry.aliases.includes(command));
+
+    // Check if it's a registered command.
+    if (cmd) {
+      cmd.execute(message, args, client);
+    }
+  } catch (error) {
+    console.error(error);
+    message.reply('Es ist ein unerwarteter Fehler aufgetreten. Goldi entschuldigt sich :(');
+  }
+};
+
 module.exports = {
-  getRandomNumber, getUserFromMention, shuffleArray, isMod,
+  getRandomNumber, getUserFromMention, shuffleArray, isMod, reactToEmojis, reactToCommands,
 };
--- src/main.js
+++ src/main.js
@@ -9,57 +9,19 @@ const fs = require('fs');
 const path = require('path');
 const Discord = require('discord.js');
 const config = require('../config.json');
+const { reactToEmojis, reactToCommands } = require('./functions');
 
+// Init discord
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+
+// Register commands.
 const commandsPath = path.resolve('./src/commands');
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-const { Cringer } = require('./entities/cringer');
-
-// Register commands
 for (const file of commandFiles) {
   const command = require(`${commandsPath}/${file}`);
   client.commands.set(command.name, command);
 }
-
-const reactToEmojis = (message) => {
-  const lowerCaseContent = message.content.toLowerCase();
-  console.log(lowerCaseContent);
-  if (lowerCaseContent.includes('kappa')) {
-    message.channel.send('https://i.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large');
-  } else if (lowerCaseContent.includes('lul')) {
-    message.channel.send('https://freepngimg.com/thumb/league_of_legends/85483-twitch-emote-face-facial-john-expression-bain.png');
-  } else if (lowerCaseContent.includes('pogchamp')) {
-    message.channel.send('https://freepngimg.com/thumb/mouth/92712-ear-head-twitch-pogchamp-emote-free-download-png-hq-thumb.png');
-  } else if (lowerCaseContent.includes('feelsbadman')) {
-    message.channel.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt9RC-fEDF3xBlO3rRUGFzdX3T25ipBlHq9Q&usqp=CAU');
-  } else if (lowerCaseContent.includes('🥳')) {
-    message.channel.send(':partying_face: :partying_face: :partying_face:');
-  }
-};
-
-const reactToCommands = (message) => {
-  // Besides reacting to emojis, ignore all non-commands.
-  if (!message.content.startsWith(config.commandPrefix)) {
-    return;
-  }
-
-  const args = message.content.slice(config.commandPrefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  try {
-    const cmd = client.commands.get(command)
-        || client.commands.find((entry) => entry.aliases && entry.aliases.includes(command));
-
-    // Check if it's a registered command.
-    if (cmd) {
-      cmd.execute(message, args, client);
-    }
-  } catch (error) {
-    console.error(error);
-    message.reply('Es ist ein unerwarteter Fehler aufgetreten. Goldi entschuldigt sich :(');
-  }
-};
 
 client.on('ready', () => {
   console.log(`Connected as ${client.user.tag}`);
@@ -74,7 +36,7 @@ client.on('message', (message) => {
   }
 
   reactToEmojis(message);
-  reactToCommands(message);
+  reactToCommands(client, message);
 });
 
 // Start the bot.
