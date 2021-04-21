ef0da432161b4e01fe6f8ab32c76fdea534f19ef
Patrick Hintermayer <Zerotask@users.noreply.github.com>
2021-04-21 13:01:54 +0200

    feat: added cron message for the main channel

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ require('./db');
 const fs = require('fs');
 const path = require('path');
 const Discord = require('discord.js');
+const cron = require('node-cron');
 const config = require('../config.json');
 const { reactToEmojis, reactToCommands } = require('./functions');
 
@@ -34,6 +35,23 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 client.on('ready', () => {
   console.log(`Connected as ${client.user.tag}`);
   client.user.setActivity(config.defaultActivity);
+
+  const mainChannel = client.channels.cache.get('575701911433510912');
+
+  // Monday reminder
+  cron.schedule('* 10 * * 1', () => {
+    mainChannel.send('Ich wünsche euch allen einen guten Start in die Woche :blush: Bleibt alle gesund!');
+  });
+
+  // Wednesday reminder
+  cron.schedule('* 14 * * 3', () => {
+    mainChannel.send('It is wednesday my dudes :frog: :frog: :frog:');
+  });
+
+  // Friday reminder
+  cron.schedule('* 18 * * 5', () => {
+    mainChannel.send('ich wünsche euch allen einen schönes Wochenende :partying_face:');
+  });
 });
 
 // Listen for incoming messages.
