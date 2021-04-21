c659ad4ac1eb3bfe6ef0a9edf1b761ca39b93a2f
Patrick Hintermayer <Zerotask@users.noreply.github.com>
2021-04-21 17:06:11 +0200

    fix(cron): changed minute from * to 0 and added timezone

--- src/main.js
+++ src/main.js
@@ -39,23 +39,28 @@ client.on('ready', () => {
   const mainChannel = client.channels.cache.get('575701911433510912');
   // const botTestingChannel = client.channels.cache.get('803680168991916112');
 
+  const cronOptions = {
+   timezone: "Europe/Berlin"
+ }
+
   // Monday reminder
-  cron.schedule('* 10 * * 1', () => {
-    mainChannel.send('Ich wünsche euch allen einen guten Start in die Woche :blush: Bleibt alle gesund!');
-  });
+  cron.schedule('0 10 * * 1', () => {
+    // mainChannel.send('Ich wünsche euch allen einen guten Start in die Woche :blush: Bleibt alle gesund!');
+  }, cronOptions);
 
   // Wednesday reminder
-  cron.schedule('* 14 * * 3', () => {
+  cron.schedule('0 14 * * 3', () => {
     mainChannel.send('It is wednesday my dudes :frog: :frog: :frog:');
+    console.log('It is wednesday my dudes :frog: :frog: :frog:');
   });
 
   // Friday reminder
-  cron.schedule('* 18 * * 5', () => {
+  cron.schedule('0 18 * * 5', () => {
     mainChannel.send('ich wünsche euch allen einen schönes Wochenende :partying_face:');
-  });
+  }, cronOptions);
 
   // Sunday reminder
-  cron.schedule('* 20 * * 7', () => {
+  cron.schedule('0 20 * * 7', () => {
     const info = [];
     info.push('ich hoffe, dass ihr alle eine tolle Woche hattet! :blush:');
     info.push('Falls ihr wissen wollt, mit welchen Kommandos ihr mich benutzen könnt, schreibt einfach !commands in den Chat.');
@@ -63,7 +68,7 @@ client.on('ready', () => {
     info.push('Du bist eher der cringe Typ? Dann komm doch in den Channel <#833389402532216873>. Für weitere Information !cringer help');
     info.push('Die Regeln findet ihr hier: <#803677596898164786>. Falls jemand dagegen verstößt oder euch belästigt, könnt ihr mit !report denjenigen melden, z. B. `!report @Goldi hat mir unanständige Nachrichten geschickt`');
     mainChannel.send(info);
-  });
+  }, cronOptions);
 });
 
 // Listen for incoming messages.
