b0e9388cc52a657d22cfee71729c91328199b68e
Zerotask <Zerotask@users.noreply.github.com>
2021-04-23 17:49:38 +0200

    chore(suggest-game): added AoE 2 (4)

--- src/commands/suggest-game.js
+++ src/commands/suggest-game.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'suggest-game',
   description: 'Goldi schlägt dir ein zufälliges Spiel vor',
   execute(message) {
-    const gameList = ['WoW', 'GTA RP', 'EU4', 'CK3', 'WoW'];
+    const gameList = ['WoW', 'GTA RP', 'EU4', 'CK3', 'AoE 2 (4)'];
     message.channel.send(`Goldi schlägt dir ${gameList[getRandomNumber(0, gameList.length)]} vor :)`);
   },
 };
