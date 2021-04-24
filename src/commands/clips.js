33437c2a978fcc1a5e488a777b33c96de80199f8
Zerotask <Zerotask@users.noreply.github.com>
2021-04-24 22:01:51 +0200

    chore(clips): added youtube link

--- src/commands/clips.js
+++ src/commands/clips.js
@@ -6,6 +6,7 @@ module.exports = {
     const output = [];
     output.push('Alle Clips findest du hier: https://www.twitch.tv/goldman94/videos?filter=clips&range=all');
     output.push('Besondere Clips werden außerdem im Chanel <#811752663359684618> gepostet.');
+    output.push('Den neuen YoutTube-Kanel findest du hier: https://www.youtube.com/user/Darkenemy22');
     message.channel.send(output);
   },
 };
