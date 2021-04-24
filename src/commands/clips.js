ed2e06883ad5b7199e3f8f47c5abf8f118de439d
Zerotask <Zerotask@users.noreply.github.com>
2021-04-24 22:25:16 +0200

    fix(clips): typo and channel fix

--- src/commands/clips.js
+++ src/commands/clips.js
@@ -6,9 +6,9 @@ module.exports = {
   description: 'Alle Clips vom Stream',
   execute(message) {
     const output = [];
-    output.push('Alle Clips findest du hier: https://www.twitch.tv/goldman94/videos?filter=clips&range=all');
-    output.push(`Besondere Clips werden außerdem im Chanel <#${config.channels.streamWeekPlan}> gepostet.`);
-    output.push('Den neuen YoutTube-Kanel findest du hier: https://www.youtube.com/user/Darkenemy22');
+    output.push('Alle Clips findest du hier: https://www.twitch.tv/goldman94/videos?filter=clips&range=all :movie_camera:');
+    output.push(`Besondere Clips werden außerdem im Chanel <#${config.channels.clips}> gepostet.`);
+    output.push('Den neuen YouTube-Kanel findest du hier: https://www.youtube.com/user/Darkenemy22');
     message.channel.send(output);
   },
 };
