89c21b5cadcca145d85f40f8ef11f56dfafdb220
Zerotask <Zerotask@users.noreply.github.com>
2021-04-25 16:05:58 +0200

    chore: update ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'ping',
   description: 'Ping!',
   execute(message, args, client) {
-    message.channel.send(`Ping: ${client.ws.ping}ms`).then((sent) => {
-      message.channel.send(`Roundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms`);
+    message.channel.send(`Ping (ICMP Package): ${client.ws.ping}ms`).then((sent) => {
+      message.channel.send(`Round Trip Time (RTT): ${sent.createdTimestamp - message.createdTimestamp}ms`);
     });
   },
 };
