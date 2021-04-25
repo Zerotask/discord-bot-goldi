61b36fb518364b30154239254380d1b498a9e9ba
Zerotask <Zerotask@users.noreply.github.com>
2021-04-25 22:07:29 +0200

    chore(clips): added tiktok channel

--- src/commands/clips.js
+++ src/commands/clips.js
@@ -8,7 +8,9 @@ module.exports = {
     const output = [];
     output.push('Alle Clips findest du hier: https://www.twitch.tv/goldman94/videos?filter=clips&range=all :movie_camera:');
     output.push(`Besondere Clips werden außerdem im Chanel <#${config.channels.clips}> gepostet.`);
+    output.push('');
     output.push('Den neuen YouTube-Kanel findest du hier: https://www.youtube.com/user/Darkenemy22');
+    output.push('Den neuen TikTok-Kanel findest du hier: https://vm.tiktok.com/ZMeQMxc8x/');
     message.channel.send(output);
   },
 };
