eae72780eb0e2d0c22d30c10128f896ae4082b24
Zerotask <Zerotask@users.noreply.github.com>
2021-05-23 14:44:15 +0200

    chore: added text to !gta command

--- src/commands/gta.js
+++ src/commands/gta.js
@@ -3,6 +3,6 @@ module.exports = {
   aliases: ['rp'],
   description: 'Informationen über GTA V Roleplay',
   execute({ message }) {
-    message.channel.send('Goldman94 spielt den Staatsanwalt Anthony Goldman (30) auf dem GTA V Roleplay Server https://dirty-gaming.com. Außerdem schaut er sich gerade auf dem GTA RP Server https://www.luckyv.de um. Dort spielt er einen Zivilist.');
+    message.channel.send('Goldman94 spielt den Staatsanwalt **Anthony Goldman** (30) auf dem GTA V Roleplay Server https://dirty-gaming.com. Außerdem schaut er sich gerade auf dem GTA RP Server https://www.luckyv.de um. Dort spielt er einen gleichnamigen Zivilisten. Kennst du auch schon den `!luckyv` command?');
   },
 };
