ebaa9e984bdc79cb6dfea76679dbe015ff7ac25a
Zerotask <Zerotask@users.noreply.github.com>
2021-08-08 21:29:20 +0200

    chore(sponti): added additional output

--- src/commands/sponti.js
+++ src/commands/sponti.js
@@ -4,6 +4,14 @@ module.exports = {
   name: 'sponti',
   description: 'Gibt es heute noch einen spontanen Stream? Goldi verräts dir ;)',
   execute({ message }) {
-    message.channel.send(`Es gibt heute zu ${getRandomNumber(0, 100)}% noch einen Sponti.`);
+    const chance = getRandomNumber(0, 100);
+    message.channel.send(`Es gibt heute zu ${chance}% noch einen Sponti.`);
+    if (chance < 40) {
+      message.channel.send('Ich glaube, das wird heute nichts mehr :cry:');
+    } else if (chance >= 40 && chance < 80) {
+      message.channel.send('Die Chancen stehen ganz gut :thinking:');
+    } else if (chance >= 80) {
+      message.channel.send('Er wird sicherlich jeden Moment den Stream starten :partying_face:');
+    }
   },
 };
