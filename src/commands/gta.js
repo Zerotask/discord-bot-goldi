e7c3458079090be79c477181ed6cc223477e648d
Zerotask <Zerotask@users.noreply.github.com>
2021-08-08 22:33:34 +0200

    chore: enhanced gta command
    added streams counter and status info

--- src/commands/gta.js
+++ src/commands/gta.js
@@ -1,15 +1,18 @@
 const fetch = require('node-fetch');
+const { parse } = require('node-html-parser');
 const { numberFormat } = require('../functions');
 
 module.exports = {
   name: 'gta',
-  aliases: ['hs', 'homestate'],
+  aliases: ['hs', 'homestate', 'hs-test'],
   description: 'Informationen über den GTA V Roleplay-Server HomeState',
   async execute({ message }) {
     let results;
     try {
       results = await Promise.all([
         fetch('https://api.altv.mp/servers/list'),
+        fetch('https://homestate.statuspage.io'),
+        fetch('https://www.homestate.eu/index.php?ha-streaming-partner/'),
       ]);
     } catch (error) {
       console.log({ error });
@@ -18,16 +21,19 @@ module.exports = {
     }
 
     const altvData = await results[0].json();
-    // const luckyVStreamersData = await results[1].json();
-
     const altvServer = altvData.find((entry) => entry.name.includes('HomeState'));
+
+    const homeStateStatusPage = await results[1].text();
+    const serverStatus = parse(homeStateStatusPage).querySelector('.page-status > .status').textContent.trim();
+
+    const homeStateWebsite = await results[2].text();
+    const streamCounter = parse(homeStateWebsite).querySelector('.boxMenuLink > .badgeUpdate').textContent.trim();
+
     const output = [];
     output.push('**HomeState - GTA V Roleplay - https://www.homestate.eu/forum/**');
     output.push(`Spieler online: **${numberFormat(altvServer.players)}**`);
-    output.push('Stream-Übersicht: https://www.homestate.eu/index.php?ha-streaming-partner/&pageNo=1&sortField=twitchPartnerLastStream&sortOrder=DESC');
-    // output.push(`Streams online: **${numberFormat(luckyVStreamersData.online)}**
-    // (${numberFormat(luckyVStreamersData.viewer)} Zuschauer)`);
-    // output.push(`Streams offline: ${numberFormat(luckyVStreamersData.offline)}`);
+    output.push(`Streams online: ${streamCounter ?? 0} (https://tinyurl.com/homestate-streams)`);
+    output.push(`Server-Status: ${serverStatus ?? 'unbekannt'} (https://homestate.statuspage.io)`);
     output.push(`alt:V-Version: ${altvServer.version}`);
     output.push('Einreise: 18.08.2021 (illegal über das Gefängnis)');
     output.push('Charakter: (Name noch unbekannt), (Alter noch unbekannt), Crime');
