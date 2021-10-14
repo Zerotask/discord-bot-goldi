57933053d91e75c18ab7ef6d18c8e319bdee18af
Zerotask <Zerotask@users.noreply.github.com>
2021-10-14 21:24:49 +0200

    chore: updated gta command

--- src/commands/gta.js
+++ src/commands/gta.js
@@ -1,42 +1,12 @@
-const fetch = require('node-fetch');
-const { parse } = require('node-html-parser');
-const { numberFormat } = require('../functions');
-
 module.exports = {
   name: 'gta',
   aliases: ['hs', 'homestate'],
   description: 'Informationen über den GTA V Roleplay-Server HomeState',
   async execute({ message }) {
-    let results;
-    try {
-      results = await Promise.all([
-        fetch('https://api.altv.mp/servers/list'),
-        fetch('https://homestate.statuspage.io'),
-        fetch('https://www.homestate.eu/index.php?ha-streaming-partner/'),
-      ]);
-    } catch (error) {
-      console.log({ error });
-      message.channel.send('Die Daten können derzeit nicht abgerufen werden. :( Versuche es später noch einmal.');
-      return;
-    }
-
-    const altvData = await results[0].json();
-    const altvServer = altvData.find((entry) => entry.name.includes('HomeState'));
-
-    const homeStateStatusPage = await results[1].text();
-    const serverStatus = parse(homeStateStatusPage).querySelector('.page-status > .status').textContent.trim();
-
-    const homeStateWebsite = await results[2].text();
-    const streamCounter = parse(homeStateWebsite).querySelector('.boxMenuLink > .badgeUpdate').textContent.trim();
-
     const output = [];
-    output.push('**HomeState - GTA V Roleplay - https://www.homestate.eu/forum/**');
-    output.push(`Spieler online: **${numberFormat(altvServer.players)}**`);
-    output.push(`Streams online: ${streamCounter ?? 0} (https://www.homestate.eu/index.php?streaming-partner/&pageNo=1&sortField=lastStreamTime&sortOrder=DESC)`);
-    output.push(`Server-Status: ${serverStatus ?? 'unbekannt'} (https://homestate.statuspage.io)`);
-    output.push(`alt:V-Version: ${altvServer.version}`);
-    output.push('Einreise: 02.09.2021 (illegal über die JVA)');
-    output.push('Charakter: Adriano Moretti, Si­zi­li­a­ner, 27 Jahre, Mafia');
+    output.push('**FamilyLife - GTA V Roleplay - https://twitter.com/FamilyLifeRP**');
+    output.push('Einreise: 04.10.2021 (legal)');
+    output.push('Charakter: Anthony Goldman aka Ronny, Sachse, 27 Jahre');
     message.channel.send(output);
   },
 };
